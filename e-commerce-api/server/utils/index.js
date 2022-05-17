eabd044a8d4f0eec1d3ae626eb5d6c656c6838b0
Aleksandr Kadulin <alexandr-kadulin@hotmail.com>
2022-05-17 13:42:18 +0300

    initial commit

---
+++ e-commerce-api/server/utils/index.js
@@ -0,0 +1,17 @@
+const { createJWT, isTokenValid, attachCookiesToResponse } = require("./jwt");
+const createTokenUser = require("./createTokenUser");
+const checkPermissions = require("./checkPermissions");
+const sendVerficationEmail = require("./sendVerficationEmail");
+const sendResetPasswordEmail = require("./sendResetPasswordEmail");
+const createHash = require("./createHash");
+
+module.exports = {
+  createJWT,
+  isTokenValid,
+  attachCookiesToResponse,
+  createTokenUser,
+  checkPermissions,
+  sendVerficationEmail,
+  sendResetPasswordEmail,
+  createHash,
+};
